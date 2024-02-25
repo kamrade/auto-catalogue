582aa39d921e3bb185ea0e391d4378aacc2bec6a
Dennis Michailov <denis.michailov@gmail.com>
2024-02-26 00:06:13 +0200

    Checkbox + focus

--- src/lib/Checkbox/Checkbox.svelte
+++ src/lib/Checkbox/Checkbox.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
 	export let value: boolean = false;
-	export let setValue: (v: boolean) => void = (v: boolean) => (value = v);
+	export let onClick: (v: boolean) => void = (v: boolean) => (value = v);
 </script>
 
 <label class="Checkbox">
+	{#if $$slots.prefix}
+		<span class="CheckboxLabel"><slot name="prefix" /></span>
+	{/if}
 	<span class={`CheckboxIndicator CheckboxIndicator--${value ? 'on' : 'off'}`}></span>
-	<input class="Checkbox--el" checked={value} type="checkbox" on:click={() => setValue(!value)} />
-	<span class="CheckboxLabel">
-		<slot />
-	</span>
+	<input class="Checkbox--el" checked={value} type="checkbox" on:click={() => onClick(!value)} />
+	{#if $$slots.default}
+		<span class="CheckboxLabel"><slot /></span>
+	{/if}
 </label>
 
 <style lang="scss">
@@ -16,17 +19,51 @@
 		position: relative;
 		display: flex;
 		gap: 0.5em;
+		cursor: pointer;
+
+		&:focus-within {
+			.CheckboxIndicator {
+				box-shadow: 0 0 0 3px var(--focus-color);
+			}
+		}
+
+		&:hover {
+			.CheckboxIndicator {
+				transform: scale(1.1);
+			}
+		}
 
 		.CheckboxIndicator {
 			display: block;
+			position: relative;
 			width: 0.625em;
 			height: 0.625em;
+			min-width: 0.625em;
+			min-height: 0.625em;
 			margin: 0.375em 0;
-			border: 1px solid #ccc;
+			border: 1px solid var(--line-control-100);
+			transform-origin: 50% 50%;
+			transition: all var(--animations-speed) ease-in-out;
+
+			&::after {
+				content: '';
+				position: absolute;
+				top: -1px;
+				right: -1px;
+				bottom: -1px;
+				left: -1px;
+				transform-origin: 50% 50%;
+				transform: scale(0);
+				opacity: 0;
+				background: var(--text-color-base);
+				transition: all var(--animations-speed) ease-in-out;
+			}
 
 			&.CheckboxIndicator--on {
-				background: #000;
-				border-color: transparent;
+				&::after {
+					transform: scale(1);
+					opacity: 1;
+				}
 			}
 		}
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,10 @@
 
 <h1>Welcome to SvelteKit</h1>
 
+<div style="padding: 12px 0;">
+	<Checkbox>Disable</Checkbox>
+</div>
+
 <Button props={{ theme: 'primary', variant: 'contained', loading: isLoading }}>
 	<i class="ri-arrow-left-up-line" slot="prefix" />
 	Primary Contained Button with Prefix Icon
@@ -16,8 +20,19 @@
 	Primary Outlined Button
 </Button>
 
+<div style="padding: 12px 0; max-width: 300px">
+	<Checkbox>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras fermentum tincidunt pulvinar.
+		Donec finibus semper sapien a tempus. Sed varius, lorem vel ultrices fringilla, nibh nisi
+		elementum mauris, id mollis purus lorem et nisl. Etiam varius orci et sagittis tincidunt.
+		Quisque blandit ipsum quis blandit volutpat. Pellentesque feugiat iaculis felis. Proin vitae
+		libero non orci mattis efficitur. Suspendisse rutrum aliquet vulputate. Aliquam viverra a nibh
+		et ultricies.
+	</Checkbox>
+</div>
+
 <div style="padding: 12px 0">
-	<Checkbox>Test</Checkbox>
+	<Checkbox><svelte:fragment slot="prefix">Prefix</svelte:fragment></Checkbox>
 </div>
 
 <style lang="scss">
