517bf5d58900a8e8e44be3a68a7f6b6427503f38
Dennis Michailov <denis.michailov@gmail.com>
2024-04-03 11:53:11 +0300

    Remove Modal from Gallery

--- src/routes/showcase/gallery-modal/+page.svelte
+++ src/routes/showcase/gallery-modal/+page.svelte
@@ -1,25 +1,12 @@
 <script lang="ts">
-  import { Gallery, type IGalleryPhoto, Modal, Button } from "$lib";
+  import { Gallery, type IGalleryPhoto, Button } from "$lib";
   import { galleryPhotos } from "./gallery-data";
-
-  let isModalVisible = false;
-
-  let hideModal = () => (isModalVisible = false);
-  let showModal = () => (isModalVisible = true);
 
   let thumbnailsPath = "http://cat.primavistalab.com/images/thumbnail-w200";
   // let imagesStorage = "http://cat.primavistalab.com/images";
 </script>
 
 <h1>Gallery and Modal</h1>
-
-<div class="mb-3">
-  <Button props={{ onClick: () => (isModalVisible = true) }}>Show Modal</Button>
-</div>
-
-{#if isModalVisible}
-  <Modal isVisible={isModalVisible}></Modal>
-{/if}
 
 <div>
   <Gallery images={galleryPhotos} {thumbnailsPath} />
