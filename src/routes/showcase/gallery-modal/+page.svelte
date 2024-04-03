688edbfb066b386c9106b209590f580f96b85fd2
Dennis Michailov <denis.michailov@gmail.com>
2024-04-03 12:18:04 +0300

    Add modal

--- src/routes/showcase/gallery-modal/+page.svelte
+++ src/routes/showcase/gallery-modal/+page.svelte
@@ -1,12 +1,24 @@
 <script lang="ts">
-  import { Gallery, type IGalleryPhoto, Button } from "$lib";
+  import { Gallery, type IGalleryPhoto, Button, Modal } from "$lib";
   import { galleryPhotos } from "./gallery-data";
+
+  let isModalShowed = false;
+  const showModal = () => (isModalShowed = true);
+  const hideModal = () => (isModalShowed = false);
 
   let thumbnailsPath = "http://cat.primavistalab.com/images/thumbnail-w200";
   // let imagesStorage = "http://cat.primavistalab.com/images";
 </script>
 
 <h1>Gallery and Modal</h1>
+
+<div class="mb-3">
+  <Button props={{ onClick: () => showModal() }}>Show Modal</Button>
+</div>
+
+{#if isModalShowed}
+  <Modal isVisible={isModalShowed} {hideModal}>Test</Modal>
+{/if}
 
 <div>
   <Gallery images={galleryPhotos} {thumbnailsPath} />
