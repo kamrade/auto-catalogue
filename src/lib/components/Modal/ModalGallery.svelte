092c5b4f5bf79d84bb96cdcc92522bf6d889633f
Dennis Michailov <denis.michailov@gmail.com>
2024-06-13 00:24:26 +0300

    Add preloader to images

--- src/lib/components/Modal/ModalGallery.svelte
+++ src/lib/components/Modal/ModalGallery.svelte
@@ -7,6 +7,9 @@
   export let currentImage: number | null;
 
   let imagePath: string;
+  let thumbnailPath: string;
+  let imageEl: any;
+  let isLoading = true;
 
   const keyUpHandler = (e: KeyboardEvent) => {
     if (e.key === 'ArrowRight') {
@@ -20,6 +23,7 @@
 
   onMount(() => {
     document.addEventListener('keyup', keyUpHandler);
+    imageEl.onload = () => isLoading = false;
   });
 
   onDestroy(() => {
@@ -28,15 +32,18 @@
 
   $: {
     imagePath = `${imagesPath}/${images[currentImage || 0].photo_name}.jpg`;
+    thumbnailPath = `${thumbnailsPath}/${images[currentImage || 0].photo_name}.jpg`;
   }
 
   const nextImage = () => {
     if (images && currentImage !== null) {
+      isLoading = true;
       currentImage = currentImage >= images.length - 1 ? 0 : currentImage + 1;
     }
   };
   const prevImage = () => {
     if (images && currentImage !== null) {
+      isLoading = true;
       currentImage = currentImage === 0 ? images.length - 1 : currentImage - 1;
     }
   };
@@ -46,7 +53,7 @@
 
   <div class="image-wrapper">
     <a href={imagePath} target="_blank" tabindex="-1">
-      <img class="modal-gallery-image" src={imagePath} alt="" />
+      <img bind:this={imageEl} class="modal-gallery-image" src={imagePath} alt="" />
     </a>
   </div>
 
@@ -54,6 +61,15 @@
     <div class="Gallery-buttons-lg">
       <div role="button" tabindex="-1" on:mouseup={prevImage} class="Gallery-button-lg Gallery-button-previous"><i class="ri-arrow-left-s-line"></i></div>
       <div role="button" tabindex="-1" on:mouseup={nextImage} class="Gallery-button-lg Gallery-button-next"><i class="ri-arrow-right-s-line"></i></div>
+    </div>
+  {/if}
+
+  {#if isLoading }
+<!--    <div class="thumbnail-preloader" style={`background-image: url(${thumbnailPath})`}></div>-->
+    <div class="preloader-text">
+      <div class="preloader-text-content">
+        Loading...
+      </div>
     </div>
   {/if}
 
@@ -141,6 +157,37 @@
       &.Gallery-button-next {
         right: .5rem;
       }
+    }
+  }
+
+  .thumbnail-preloader {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+    display: flex;
+    background-repeat: no-repeat;
+    background-size: cover;
+    filter: blur(40px);
+  }
+
+  .preloader-text {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+    display: flex;
+
+    .preloader-text-content {
+      margin: auto;
+      color: white;
+      padding: 100px;
+      border-radius: 20px;
+      background-color: rgba(0, 0, 0, 0.7);
     }
   }
 </style>
